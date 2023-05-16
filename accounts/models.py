5bc632e2d5f6d596ca6b133c22d4a465a097ece4
Bekzod <bekzodusarovv@gmail.com>
2023-05-16 14:38:06 +0500

    1-v

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone_number = models.CharField(max_length=13, blank=False, null=False)
